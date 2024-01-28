254650891abdb1fb1490ebd2369092e9f3e06dd4
amitYadavDev <amit8955461017@gmail.com>
2024-01-28 16:46:35 +0530

    save pdf impl

--- app/src/main/java/amitapps/media/webviewtopdf/MainActivity.kt
+++ app/src/main/java/amitapps/media/webviewtopdf/MainActivity.kt
@@ -1,11 +1,38 @@
 package amitapps.media.webviewtopdf
 
-import androidx.appcompat.app.AppCompatActivity
+import amitapps.media.webviewtopdf.databinding.ActivityMainBinding
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+
 
 class MainActivity : AppCompatActivity() {
+    var _binding: ActivityMainBinding? = null
+    val binding get() = _binding
+    var printWeb: WebView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding?.webViewMain?.setWebViewClient(object : WebViewClient() {
+            override fun onPageFinished(view: WebView, url: String) {
+                super.onPageFinished(view, url)
+
+                // initializing the printWeb Object
+                printWeb = view
+            }
+        })
+
+        // loading the URL
+
+        binding?.webViewMain?.loadUrl("https://www.google.com")
+
+        savePDF()
+    }
+
+    private fun savePDF() {
+        
     }
 }
